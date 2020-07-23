10ea3bc28133be69c754ccf25170d840f646ef2b
Tymoteusz Kubicz <tku@MacBook-Pro.local>
2020-07-23 16:11:02 +0200

    Add impl of std Error for GetFromUrlError

--- server/src/utils/getFromUrl.rs
+++ server/src/utils/getFromUrl.rs
@@ -1,9 +1,11 @@
-
 //responseHtml(500, format!("Error build http client {:?}", err))
 //responseHtml(500, format!("Error send {:?}", err))
 //responseHtml(500, format!("Error get text {:?}", err))
 
+use std::error::Error;
+use std::fmt;
 
+#[derive(Debug)]
 pub enum GetFromUrlError {
     ReqwestBuildClient { err: String },
     ReqwestErrorSend { err: String },
@@ -13,13 +15,34 @@ pub enum GetFromUrlError {
 impl GetFromUrlError {
     pub fn toString(&self) -> String {
         match self {
-            GetFromUrlError::ReqwestBuildClient { err } => format!("CrealerError::ReqwestBuildClient (err: {})", err),
-            GetFromUrlError::ReqwestErrorSend { err } => format!("CrealerError::ReqwestErrorSend (err: {})", err),
-            GetFromUrlError::ReqwestGetError { err } => format!("CrealerError::ReqwestGetError (err: {})", err),
+            GetFromUrlError::ReqwestBuildClient { err } => {
+                format!("CrealerError::ReqwestBuildClient (err: {})", err)
+            }
+            GetFromUrlError::ReqwestErrorSend { err } => {
+                format!("CrealerError::ReqwestErrorSend (err: {})", err)
+            }
+            GetFromUrlError::ReqwestGetError { err } => {
+                format!("CrealerError::ReqwestGetError (err: {})", err)
+            }
         }
     }
 }
 
+impl fmt::Display for GetFromUrlError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.toString())
+    }
+}
+
+impl Error for GetFromUrlError {
+    fn description(&self) -> &str {
+        match self {
+            GetFromUrlError::ReqwestBuildClient { err } => err,
+            GetFromUrlError::ReqwestErrorSend { err } => err,
+            GetFromUrlError::ReqwestGetError { err } => err,
+        }
+    }
+}
 
 pub async fn getFromUrl(url: &str) -> Result<String, GetFromUrlError> {
     let builder = reqwest::Client::builder()
@@ -30,7 +53,9 @@ pub async fn getFromUrl(url: &str) -> Result<String, GetFromUrlError> {
     let client = match client {
         Ok(client) => client,
         Err(err) => {
-            return Err(GetFromUrlError::ReqwestBuildClient { err: format!("{}", err)});
+            return Err(GetFromUrlError::ReqwestBuildClient {
+                err: format!("{}", err),
+            });
         }
     };
 
@@ -39,7 +64,9 @@ pub async fn getFromUrl(url: &str) -> Result<String, GetFromUrlError> {
     let resp = match resp {
         Ok(resp) => resp,
         Err(err) => {
-            return Err(GetFromUrlError::ReqwestErrorSend { err: format!("{}", err)});
+            return Err(GetFromUrlError::ReqwestErrorSend {
+                err: format!("{}", err),
+            });
         }
     };
 
@@ -48,7 +75,9 @@ pub async fn getFromUrl(url: &str) -> Result<String, GetFromUrlError> {
     let resp = match resp {
         Ok(resp) => resp,
         Err(err) => {
-            return Err(GetFromUrlError::ReqwestGetError { err: format!("{}", err)});
+            return Err(GetFromUrlError::ReqwestGetError {
+                err: format!("{}", err),
+            });
         }
     };
 
